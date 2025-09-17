416b1e6f38ccb84583c6f7bc75068213700e3816
Playergeek181 <joneair131@gmail.com>
2025-09-16 23:29:06 -0300

    ROSbrigde Lidar PX4 sensor

---
+++ src/nodes/px4_lidar_bridge_node.cpp
@@ -0,0 +1,15 @@
+#include "../../include/PX4_lidar_bridge/PX4_lidar_bridge.h"
+
+using namespace bridge;
+
+int main(int argc, char** argv) {
+  ros::init(argc, argv, "PX4_lidar_NRA_bridge_node");
+  ros::NodeHandle nh("~");
+  PX4_Realsense_Bridge Bridge(nh);
+
+  ros::spin();
+
+  Bridge.worker_.join();
+
+  return 0;
+}
